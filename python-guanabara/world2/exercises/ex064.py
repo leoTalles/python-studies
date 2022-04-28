fb43e3891b2922d2e262fafbc99b368c41227564
leoTalles <leotalles16@gmail.com>
2022-04-28 20:09:55 -0300

    up ex063; ex064

--- python-guanabara/world2/exercises/ex064.py
+++ python-guanabara/world2/exercises/ex064.py
@@ -1,6 +1,7 @@
 # Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999
 # que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles
 # (Desconsiderando o flag(999))
+#TODO VOLTAR AQUI DEPOIS DA AULA 15
 n = 0
 c = 0
 soma = 0
--- python-guanabara/world2/exercises/ex065.py
+++ python-guanabara/world2/exercises/ex065.py
@@ -1,3 +1,12 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução mostre a média entre todos
 # os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não 
 # continuar a digitar valores
+#TODO VOLTAR AQUI DEPOIS DA AULA 15
+n = 0
+c = 0
+
+while n :
+    n = int(input('Insira números caralho: '))
+    c += 1
+
+print('fim')
