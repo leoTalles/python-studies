069bb4928803398d5f60da6512e06c13422d53ab
leoTalles <leotalles16@gmail.com>
2022-04-28 20:47:33 -0300

    fix2 ex065

--- python-guanabara/world2/exercises/ex065.py
+++ python-guanabara/world2/exercises/ex065.py
@@ -5,13 +5,23 @@
 
 num = 0
 resp = 'S' 
-soma = media = count = 0
+soma = media = count = maior = menor = 0
 
 while resp in 'Ss':
     num = int(input('Insira um número: '))
     soma += num
     count += 1
     resp = str(input('Deseja continuar? [S/N]: ')).upper().strip()[0]
+    if num == 1:
+        maior = num
+        menor = num
+    else:
+        if num > maior:
+            maior = num    
+        if num < menor:
+            menor = num
 media = soma / count
 print('Você digitou {} números e a soma é igual a {}'.format(count, soma))
 print('O resultado da média é igual a ', media)
+print('O menor numero é ', menor)
+print('O maior numero é ', maior)
