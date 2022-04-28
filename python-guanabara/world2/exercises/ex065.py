0840eb1ac94285f22604ad6fa6efab00cfb25c0c
leoTalles <leotalles16@gmail.com>
2022-04-28 20:38:46 -0300

    fix ex065

--- python-guanabara/world2/exercises/ex065.py
+++ python-guanabara/world2/exercises/ex065.py
@@ -2,11 +2,16 @@
 # os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não 
 # continuar a digitar valores
 #TODO VOLTAR AQUI DEPOIS DA AULA 15
-n = 0
-c = 0
 
-while n :
-    n = int(input('Insira números caralho: '))
-    c += 1
+num = 0
+resp = 'S' 
+soma = media = count = 0
 
-print('fim')
+while resp in 'Ss':
+    num = int(input('Insira um número: '))
+    soma += num
+    count += 1
+    resp = str(input('Deseja continuar? [S/N]: ')).upper().strip()[0]
+media = soma / count
+print('Você digitou {} números e a soma é igual a {}'.format(count, soma))
+print('O resultado da média é igual a ', media)
